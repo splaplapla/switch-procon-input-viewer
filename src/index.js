9a51a47e8fd20a8998232300790fe0dcb5915184
jiikko <n905i.1214@gmail.com>
2022-05-24 23:50:44 +0900

    a

--- src/index.js
+++ src/index.js
@@ -6,5 +6,27 @@ function component() {
 
   return element;
 }
-
 document.body.appendChild(component());
+
+const Net = require('net');
+const port = 9900;
+const host = '192.168.50.122';
+
+function connectToServer(port, host) {
+  const client = new Net.Socket();
+
+  client.connect(port, host, () => {
+    // no-op
+  });
+
+  client.on('data', data => {
+    console.log('client-> ' + data);
+    client.destroy();
+  });
+
+  client.on('close', () => {
+    console.log('connection is closed');
+  });
+}
+
+connectToServer(port, host)
