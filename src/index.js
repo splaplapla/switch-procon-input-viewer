162ff62b8d9f9329b39fb350bab8ee45c073a735
jiikko <n905i.1214@gmail.com>
2022-05-25 16:40:10 +0900

    hai

--- src/index.js
+++ src/index.js
@@ -1,32 +1,25 @@
 function component() {
   const element = document.createElement('div');
-
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
   return element;
 }
 document.body.appendChild(component());
 
-const Net = require('net');
+const axios = require('axios');
 const port = 9900;
 const host = '192.168.50.122';
 
-function connectToServer(port, host) {
-  const client = new Net.Socket();
+function fetchProcon(port, host) {
+  (async () => {
+    try {
+      const response = await axios.get(`http://${host}:${port}`)
+      console.log(response.data.url);
+      console.log(response.data.explanation);
+    } catch (error) {
+      console.log(error.response.body);
+    }
+  })();
+};
 
-  client.connect(port, host, () => {
-    // no-op
-  });
 
-  client.on('data', data => {
-    console.log('client-> ' + data);
-    client.destroy();
-  });
-
-  client.on('close', () => {
-    console.log('connection is closed');
-  });
-}
-
-connectToServer(port, host)
+fetchProcon(port, host)
