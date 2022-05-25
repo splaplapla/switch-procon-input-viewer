1ef15f518a8c2e38eb31cf5b9d84e178995c7156
jiikko <n905i.1214@gmail.com>
2022-05-25 19:19:16 +0900

    add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# SwitchProControllerInputViewer
+* 入力表示ができるWEBページです
+* https://github.com/splaplapla/procon_bypass_man の 0.2.1 が必要です
+
+## 開発
+* npm ci
+* npm run server
+* http://localhost:8080/ を開く
--- src/pages/top.jsx
+++ src/pages/top.jsx
@@ -4,8 +4,14 @@ import ReactDOM from "react-dom";
 export const Top = () => {
   return(
     <>
-      <div>foooooo2</div>
+      <div>
+        接続先IPアドレス: <input type="text" value="http://192.168.50.122:9900/" />
+      </div>
+      <div>
+        <label>
+        状態を取得する: <input type="checkbox" />
+        </label>
+      </div>
     </>
   );
 }
-
